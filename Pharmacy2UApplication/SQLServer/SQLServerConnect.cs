e3b5f478f8971273753c1fa85d78b2e50d084cff
Jim Allen <iggalot.iggy@gmail.com>
2019-07-01 22:55:58 -0500

    Database activity window is now active.  Added "History" feature for the database activity in SQLServerConnect.cs.

--- Pharmacy2UApplication/SQLServer/SQLServerConnect.cs
+++ Pharmacy2UApplication/SQLServer/SQLServerConnect.cs
@@ -1,5 +1,7 @@
 ﻿
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Input;
@@ -8,6 +10,13 @@ namespace Pharmacy2UApplication
 {
     public class SQLServerConnect
     {
+        #region Public Members
+
+        // A list of our database command history.
+        public static ObservableCollection<string> History { get; set; }
+
+        #endregion
+
         #region Database Relay Commands
 
         // Our command to create and populate a default database
@@ -29,11 +38,20 @@ namespace Pharmacy2UApplication
         /// </summary>
         public SQLServerConnect()
         {
+            // Create our database history
+            History = new ObservableCollection<string>();
+
+
+            History.Add("Creating connection to database...");
             Console.WriteLine("Creating connection to database...");
             //MessageBox.Show("Making new Connection");
 
+
+
             // Create commands to create, display and delete the database via XAML controls.
-            Console.WriteLine("-- Creating database database commands");
+            Console.WriteLine("-- Creating database commands");
+            History.Add("-- Creating database commands");
+
             DBCreateCommand = new RelayCommand(() => this.CreateDB());
             DBClearCommand = new RelayCommand(() => this.ClearDB());
             DBDisplayCommand = new RelayCommand(() => this.DisplayDB());
@@ -48,9 +66,17 @@ namespace Pharmacy2UApplication
         /// </summary>
         private void DisplayDB()
         {
-            Console.WriteLine("-------------------------------------");
-            Console.WriteLine("----Displaying all database objects");
             int recordCount = 0;
+            string cmd = "";
+
+            cmd = "-------------------------------------";
+            Console.WriteLine(cmd);
+            History.Add(cmd);
+
+            cmd = "----Displaying all database objects";
+            Console.WriteLine(cmd);
+            History.Add(cmd);
+
             // The SQL connection string required to connect to our database.  
             // As defined at "SQL Server Connection Strings" website.
             /// Server=myServerAddress;Database=myDataBase;User Id=myUsername; Password = myPassword;
@@ -66,7 +92,10 @@ namespace Pharmacy2UApplication
                     sqlConnection.Open();
 
                     // For Reading a record:  The command to execute on our SQL server
-                    using (var command = new SqlCommand("SELECT * FROM dbo.Users", sqlConnection))
+                    cmd = "SELECT * FROM dbo.Users";
+                    History.Add(cmd);
+
+                    using (var command = new SqlCommand(cmd, sqlConnection))
                     {
                         // Read the database results that are returned
                         using (var reader = command.ExecuteReader())
@@ -74,21 +103,34 @@ namespace Pharmacy2UApplication
                             while (reader.Read())
                             {
                                 // writing out our database results
-                                Console.WriteLine($"Username: {reader["Username"]}, FirstName: {reader["FirstName"]}, LastName: {reader["LastName"]}, IsEnabled: {reader["IsEnabled"]}");
+                                cmd = $"Username: {reader["Username"]}, FirstName: {reader["FirstName"]}, LastName: {reader["LastName"]}, IsEnabled: {reader["IsEnabled"]}";
+                                History.Add(cmd);
+                                Console.WriteLine(cmd);
                                 recordCount++;
                             }
                         }
                     }
                 } catch
                 {
-                    MessageBox.Show("Error Opening database");
+                    cmd = "Error Opening database";
+                    History.Add(cmd);
+                    MessageBox.Show(cmd);
                 } finally { 
                     //Close the SQL connection
                     sqlConnection.Close();
                 }
             }
 
-            Console.WriteLine("---- Displaying " + recordCount.ToString() + " records");
+            cmd = $"---- Displaying '{recordCount.ToString()} records";
+            History.Add(cmd);
+            Console.WriteLine(cmd);
+
+            for(int i=0; i<History.Count; i++)
+            {
+                Console.WriteLine($"{i} -- '{History[i]}");
+            }
+
+
         }
 
         /// <summary>
@@ -96,8 +138,15 @@ namespace Pharmacy2UApplication
         /// </summary>
         private void ClearDB()
         {
+            string cmd = "";
+
+            cmd = "-------------------------------------";
+            History.Add(cmd);
             Console.WriteLine("-------------------------------------");
-            Console.WriteLine("----Deleting database objects");
+
+            cmd = "---- Deleting database objects";
+            History.Add(cmd);
+            Console.WriteLine("---- Deleting database objects");
 
             // The SQL connection string required to connect to our database.  
             // As defined at "SQL Server Connection Strings" website.
@@ -114,16 +163,22 @@ namespace Pharmacy2UApplication
                     sqlConnection.Open();
 
                     // For deleting all records from the database:  The command to execute on our SQL server
-                    using (var command = new SqlCommand("DELETE FROM dbo.Users", sqlConnection))
+                    cmd = "DELETE FROM dbo.Users";
+                    using (var command = new SqlCommand(cmd, sqlConnection))
                     {
+                        History.Add(cmd);
                         // Read the database results that are returned
                         int reader = command.ExecuteNonQuery();
 
-                        Console.WriteLine("---- " + reader.ToString() + " records deleted from database");
+                        cmd = $"---- '{ reader.ToString()} records deleted from database";
+                        History.Add(cmd);
+                        Console.WriteLine($"---- '{ reader.ToString()} records deleted from database");
                     }
                 } catch
                 {
-                    MessageBox.Show("Error Opening database");
+                    cmd = "Error Opening database";
+                    History.Add(cmd);
+                    MessageBox.Show(cmd);
                 }
                 finally
                 {
@@ -138,7 +193,14 @@ namespace Pharmacy2UApplication
         /// </summary>
         private void CreateDB()
         {
+            string cmd = "";
+
+            cmd = "-------------------------------------";
+            History.Add(cmd);
             Console.WriteLine("-------------------------------------");
+
+            cmd = "---- Creating database objects";
+            History.Add(cmd);
             Console.WriteLine("---- Creating database objects");
 
             // The SQL connection string required to connect to our database.  
@@ -157,39 +219,54 @@ namespace Pharmacy2UApplication
                     int recordCount = 0;
 
                     // For Writing a record:  The command to execute on our SQL server
-                    using (var command = new SqlCommand($"INSERT INTO dbo.Users (id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Jim', 'Jim', 'Allen', 1, '2/28/19 1:45:00 AM -08:00' )", sqlConnection))
+                    cmd = $"INSERT INTO dbo.Users (id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Jim', 'Jim', 'Allen', 1, '2/28/19 1:45:00 AM -08:00' )";
+                    using (var command = new SqlCommand(cmd, sqlConnection))
                     {
-                        // Read the database results that are returned
-                        var result = command.ExecuteNonQuery();
-                        recordCount++;
-                    }
-                    // For Writing a record:  The command to execute on our SQL server
-                    using (var command = new SqlCommand($"INSERT INTO dbo.Users (id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Kira123', 'Kira', 'Lukilu', 1, '2/28/19 1:45:00 AM -08:00' )", sqlConnection))
-                    {
-                        // Read the database results that are returned
-                        var result = command.ExecuteNonQuery();
-                        recordCount++;
-                    }
-                    // For Writing a record:  The command to execute on our SQL server
-                    using (var command = new SqlCommand($"INSERT INTO dbo.Users (id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Olaf123', 'Olaf', 'von Owenson', 1, '2/28/19 1:45:00 AM -08:00' )", sqlConnection))
-                    {
-                        // Read the database results that are returned
-                        var result = command.ExecuteNonQuery();
-                        recordCount++;
-                    }
-                    // For Writing a record:  The command to execute on our SQL server
-                    using (var command = new SqlCommand($"INSERT INTO dbo.Users (id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Leslie123', 'Leslie', 'Retriever', 1, '2/28/19 1:45:00 AM -08:00' )", sqlConnection))
-                    {
+                        History.Add(cmd);
                         // Read the database results that are returned
                         var result = command.ExecuteNonQuery();
                         recordCount++;
                     }
 
-                    Console.WriteLine("---- " + recordCount.ToString() + " records added to the database");
+                    cmd = $"INSERT INTO dbo.Users (id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Kira123', 'Kira', 'Lukilu', 1, '2/28/19 1:45:00 AM -08:00' )";
+                    // For Writing a record:  The command to execute on our SQL server
+                    using (var command = new SqlCommand(cmd, sqlConnection))
+                    {
+                        History.Add(cmd);
+                        // Read the database results that are returned
+                        var result = command.ExecuteNonQuery();
+                        recordCount++;
+                    }
+
+                    // For Writing a record:  The command to execute on our SQL server
+                    cmd = $"INSERT INTO dbo.Users (id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Olaf123', 'Olaf', 'von Owenson', 1, '2/28/19 1:45:00 AM -08:00' )";
+                    using (var command = new SqlCommand(cmd, sqlConnection))
+                    {
+                        History.Add(cmd);
+                        // Read the database results that are returned
+                        var result = command.ExecuteNonQuery();
+                        recordCount++;
+                    }
+
+                    // For Writing a record:  The command to execute on our SQL server
+                    cmd = $"INSERT INTO dbo.Users (id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Leslie123', 'Leslie', 'Retriever', 1, '2/28/19 1:45:00 AM -08:00' )";
+                    using (var command = new SqlCommand(cmd, sqlConnection))
+                    {
+                        History.Add(cmd);
+                        // Read the database results that are returned
+                        var result = command.ExecuteNonQuery();
+                        recordCount++;
+                    }
+
+                    cmd = $"---- '{recordCount.ToString()} records added to the database";
+                    Console.WriteLine(cmd);
+
                 }
                 catch
                 {
-                    MessageBox.Show("Error Opening database");
+                    cmd = "Error Opening database";
+                    History.Add(cmd);
+                    MessageBox.Show(cmd);
                 }
                 finally
                 {
