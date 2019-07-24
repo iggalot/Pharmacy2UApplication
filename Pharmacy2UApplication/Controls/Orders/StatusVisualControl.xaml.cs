f0d24118c71beb0cd2e4ad6a77ac5798d480a509
Jim Allen <iggalot.iggy@gmail.com>
2019-07-24 02:04:26 -0500

    Started work on a StatusVisual control that will show differing numbers of rectangles and colors based on current status, within the OrderItemControl.  Control is created, but the core functionality has not been implemented yet.

---
+++ Pharmacy2UApplication/Controls/Orders/StatusVisualControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Pharmacy2UApplication
+{
+    /// <summary>
+    /// Interaction logic for StatusVisualControl.xaml
+    /// </summary>
+    public partial class StatusVisualControl : UserControl
+    {
+        public StatusVisualControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
